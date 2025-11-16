36cb76c7934957ec21ee32b4603dc0c2d9bdbfbd
William Howell <williamhowell786@proton.me>
2025-11-16 00:30:13 -0500

    add top five most intensive programs functionality

--- server-stats.sh
+++ server-stats.sh
@@ -1,1 +1,12 @@
+printf "CPU USAGE: "
 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'
+
+
+
+
+
+
+printf "\nMOST CPU INTENSIVE PROGRAMS: \n\n"
+ps -eo cmd --sort=-%cpu | head -n 6 | awk '{print $1}'
+printf "\nMOST MEMORY INTENSIVE PROGRAMS: \n\n"
+ps -eo cmd --sort=-%mem | head -n 6 | awk '{print $1}'
