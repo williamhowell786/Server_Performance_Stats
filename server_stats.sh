fbed51daba32bf1cac90e9f604c210787c223ac2
William Howell <williamhowell786@proton.me>
2025-11-19 12:55:34 -0500

    add shebang and rename script

--- server_stats.sh
+++ server_stats.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "TOTAL CPU USAGE: " 100 - $1"%"}'
 free -t | awk 'NR == 2 {printf("TOTAL MEMORY USAGE: %.2f%\n"), $3/$2*100}'
 df -hl --total | tail -n 1 | awk '{print "TOTAL DISK USAGE: " $5}'
